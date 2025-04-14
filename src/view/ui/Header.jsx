19a6ba6ba7f4e95036be50459fa191e67c8e0ce3
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2025-04-14 22:06:50 +0100

    feat(header): style + icons

--- src/view/ui/Header.jsx
+++ src/view/ui/Header.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import { SlBasket } from 'react-icons/sl'
 
 export const Header = () => {
     return (
-        <header className="flex h-20 items-center justify-between p-4 bg-gray-800 text-white shadow-md">
-            <Link href="/"><div>Logo</div></Link>
-            <div>
-                Icon: in store
+        <header className="flex h-20 items-center justify-between p-4 px-10 bg-gray-800 text-white shadow-md">
+            <Link href="/"><span className="text-2xl font-bold hover:text-lime-700">ACER</span></Link>
+            <div className='flex items-center gap-4 hover:cursor-pointer'>
+                <SlBasket size={30} />
+                <span className="bg-red-500 text-white rounded-full px-2 py-1 text-xs font-bold">0</span>
             </div>
 
         </header>
